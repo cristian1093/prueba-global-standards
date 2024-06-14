369e66120c0cddfa89030ea94fe9fdfacd3a199d
cristian1093 <ing.cristian_bonilla93@hotmail.com>
2024-06-14 11:06:40 -0600

    modulo de movies y schedules

---
+++ app/Repositories/Contracts/MovieRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\Data\MovieData;
+
+interface MovieRepositoryInterface
+{
+    public function getAll();
+    public function getById($id);
+    public function create(MovieData $data);
+    public function update(MovieData $data);
+    public function delete($id);
+    public function getSchedules($id);
+}
